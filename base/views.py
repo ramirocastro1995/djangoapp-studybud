a3743c189d207a34aa23be74e989b083ec0a47e7
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-09-16 21:19:23 -0300

    change in views and models creation

--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
+from unicodedata import name
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
+
+#rooms = [
+#    {'id':1, 'name':'aprender python'},
+#    {'id':2, 'name':'aprender python'},
+#    {'id':3, 'name':'aprender python'},
+#]
+
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request,'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
+    return render(request,'base/room.html', context)
