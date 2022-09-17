68ae5307f56c6f53320af92bdfc683ff394c76a6
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-09-17 18:45:41 -0300

    update database and views

--- base/models.py
+++ base/models.py
@@ -10,13 +10,18 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    #host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank = True)
     # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated','created']
+
+
 
     def __str__(self):
         return self.name
