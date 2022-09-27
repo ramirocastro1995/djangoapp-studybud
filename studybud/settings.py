e7c66aedf31283275218a6c39c089fa5de67fd05
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-09-27 09:06:29 -0300

    'update'

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
