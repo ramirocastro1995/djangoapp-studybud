0ea714029edf3b197526ea9aecdb16a6d432d77d
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-09-28 17:35:20 -0300

    update

--- studybud/settings.py
+++ studybud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
